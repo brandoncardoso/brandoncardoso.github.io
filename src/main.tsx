3b8697a0f2eda3b70ddac3a602dca4b1977dcedd
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2024-07-27 23:40:09 +0900

    add react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { MantineProvider } from '@mantine/core';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom'
+import { MantineProvider } from '@mantine/core'
+
 import App from './App.tsx'
 
 import '@mantine/core/styles.css'
 import './index.css'
 
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<App />} />
+	)
+)
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<MantineProvider>
-			<App />
+			<RouterProvider router={router} />
 		</MantineProvider>
 	</React.StrictMode>,
 )
