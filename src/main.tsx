1b32bdd8ae8dd4c827660956d8a70c124f439dbe
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2024-10-10 19:21:40 +0900

    default colorscheme auto

--- src/main.tsx
+++ src/main.tsx
@@ -63,7 +63,7 @@ const theme = createTheme({
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<MantineProvider theme={theme}>
+		<MantineProvider theme={theme} defaultColorScheme="auto">
 			<RouterProvider router={router} />
 		</MantineProvider>
 	</React.StrictMode>,
