72e7d101102862da1b0d8764eff943f5cc5e4c0e
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2024-10-10 19:33:42 +0900

    enable source maps in dev

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/',
   plugins: [react()],
-})
+  build: {
+    sourcemap: mode === 'development'
+  }
+}))
