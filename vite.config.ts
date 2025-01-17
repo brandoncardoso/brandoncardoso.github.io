f20a7a4af63e89c77d379ee041077d4a8090ba7b
Brandon Cardoso <1226170+brandoncardoso@users.noreply.github.com>
2025-01-18 05:26:20 +0900

    fix tabler-icons chunking

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,12 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig(({ mode }) => ({
   base: '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    }
+  },
   build: {
     sourcemap: mode === 'development'
   }
